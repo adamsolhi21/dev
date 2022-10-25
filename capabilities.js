6976aba124d074a692eaf32100253a22eac6eb45
AdamTravers <83338326+adamsolhi21@users.noreply.github.com>
2022-10-25 18:44:19 +0100

    Add files via upload

---
+++ capabilities.js
@@ -0,0 +1,15 @@
+exports.capabilities = {
+	"browserName": "Chrome",
+	"browserVersion": "106.0",
+	"LT:Options": {
+		"username": "adam.travers",
+		"accessKey": "3QvTjiaQwdjoXJbYH7y6EospP3nrc1NQ3I24tlqqrhyU6yKxAa",
+		"platformName": "Windows 10",
+		"project": "Untitled",
+		"w3c": true,
+		"plugin": "node_js-mocha"
+	}
+};
+module.exports = {
+  capabilities
+};
